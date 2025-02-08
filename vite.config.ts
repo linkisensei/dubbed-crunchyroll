8d2cf0cdc9ceef38c4b776cfc5c55135bd336ca6
Your Name <lucas.barreto@leolearning.com>
2025-02-08 11:31:46 -0300

    Build manual

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const defaultRepositoryName = 'dubbed-crunchyroll';
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const repositoryName = isGitHubActions ? process.env.GITHUB_REPOSITORY?.split('/')[1] : defaultRepositoryName;
+
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : `/${repositoryName}/`,
   plugins: [react()],
   resolve: {
     alias: {
@@ -15,4 +20,4 @@ export default defineConfig({
       'localhost'
     ]
   }
-})
+}));
